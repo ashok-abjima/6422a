0cc822599a170f7ddff548c4b122349d6ea5c112
Ashok Bajiya <88234185+ashok-abjima@users.noreply.github.com>
2022-04-06 23:00:22 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//GENERICS in c#
+class Stud <T,T1>
+{
+    public T i;
+    public T1 j;
+}
+class Program
+{
+    static void Main ()
+    {
+        Stud <int,string> obj = new Stud<int,String> ();
+        obj.i = 1;
+        
+        obj.j = ".Ashok";
+
+        Console.WriteLine ("{0}{1}",obj.i,obj.j);
+        Console.ReadKey ();
+    }
+}
